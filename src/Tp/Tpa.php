9ac41b20e5aa41e64a93b4cf5278adfc2aa62597
fernanACM <83558341+fernanACM@users.noreply.github.com>
2021-10-18 18:31:03 -0500

    Update Tpa.php

--- src/Tp/Tpa.php
+++ src/Tp/Tpa.php
@@ -15,7 +15,15 @@ class Tpa extends PluginBase implements Listener{
 
     public function onEnable(){
           $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getServer()->getLogger()->info("§lSimple§6TPA §aON");
+        $this->getServer()->getLogger()->info("
+  _____                      _       ____   __  __ 
+ |_   _|  _ __     __ _     / \     / ___| |  \/  |
+   | |   | '_ \   / _` |   / _ \   | |     | |\/| |
+   | |   | |_) | | (_| |  / ___ \  | |___  | |  | |
+   |_|   | .__/   \__,_| /_/   \_\  \____| |_|  |_|
+         |_|                                       
+        by fernanACM 
+        ");
         $this->askerarr = [];
         $this->vktmarr = [];
         $this->vktmarrhere = [];
@@ -48,20 +56,20 @@ class Tpa extends PluginBase implements Listener{
         switch($cmd->getName()){
             case "tpa":
             if(!isset($args[0])){
-                $sender->sendMessage("§l§cYou must specific player");
+                $sender->sendMessage("§3§l[§bTpaACM§3] §r§cYou must specific player");
             }else {
                 $player = $this->getServer()->getPlayer($args[0]);
                 if($player === null){
-                    $sender->sendMessage("§l§cThis player is not online");
+                    $sender->sendMessage("§3§l[§bTpaACM§3] §r§cThis player is not online");
                 }else {
                     if(isset($this->vktmarr[strtolower($player->getLowerCaseName())]) or isset($this->askerarr[strtolower($player->getLowerCaseName())]) or  isset($this->vktmarrhere[strtolower($player->getLowerCaseName())]) or  isset($this->askerarrhere[strtolower($player->getLowerCaseName())])){
                 
-                        $sender->sendMessage("§c§lThis player already have a request");
+                        $sender->sendMessage("§3§l[§bTpaACM§3] §r§cThis player already have a request");
                        }else{
                     $this->asker = $sender;
                     $this->vktm = $player;
-                    $player->sendMessage("§a§l".$sender->getName() . " want teleport to you \n §l§6/tpaccept for accept §5or §c/tpdeny for deny");
-                    $sender->sendMessage("§a§lYour request as been send to ". $player->getName());
+                    $player->sendMessage("§3§l[§bTpaACM§3] §r§e".$sender->getName() . " §awant teleport to you \n §l§6/tpaccept for accept §5or §c/tpadeny for deny");
+                    $sender->sendMessage("§3§l[§bTpaACM§3] §r§aYour request as been send to §e". $player->getName());
                     $this->askerarr[$sender->getLowerCaseName()] = true;
                     $this->vktmarr[$player->getLowerCaseName()] = true;
 
@@ -71,30 +79,30 @@ class Tpa extends PluginBase implements Listener{
             break;
             case "tpaccept":
             if(isset($this->vktmarr[strtolower($sender->getLowerCaseName())])){
-              $this->asker->sendMessage("§a§l".$sender->getName() . " accept your request");
-              $this->vktm->sendMessage("§a§lYou have accepted a request");
+              $this->asker->sendMessage("§3§l[§bTpaACM§3] §r§e".$sender->getName() . " §aaccept your request");
+              $this->vktm->sendMessage("§3§l[§bTpaACM§3] §r§aYou have accepted a request");
               $xyz = new Vector3($this->vktm->getX(),$this->vktm->getY(),$this->vktm->getZ()); 
               $this->asker->teleport($xyz);
               unset($this->askerarr[$this->asker->getLowerCaseName()]);
               unset($this->vktmarr[$this->vktm->getLowerCaseName()]);
             }elseif(isset($this->vktmarrhere[strtolower($sender->getLowerCaseName())])){ 
-                $this->asker->sendMessage("§a§l".$sender->getName() . " accept your request");
-                $this->vktm->sendMessage("§a§lYou have accepted a request");
+                $this->asker->sendMessage("§3§l[§bTpaACM§3] §r§e".$sender->getName() . " §aaccept your request");
+                $this->vktm->sendMessage("§3§l[§bTpaACM§3] §r§aYou have accepted a request");
                 $xyz = new Vector3($this->asker->getX(),$this->asker->getY(),$this->asker->getZ()); 
                 $this->vktm->teleport($xyz);
                 unset($this->askerarrhere[$this->asker->getLowerCaseName()]);
                 unset($this->vktmarrhere[$this->vktm->getLowerCaseName()]);
             }else{
-                $sender->sendMessage("§c§lYou dont have request");
+                $sender->sendMessage("§3§l[§bTpaACM§3] §r§cYou dont have request");
             }
             break;
             case "tpahere":
             if(!isset($args[0])){
-                $sender->sendMessage("§l§cYou must specific player");
+                $sender->sendMessage("§3§l[§bTpaACM§3] §r§cYou must specific player");
             }else {
                 $player = $this->getServer()->getPlayer($args[0]);
                 if($player === null){
-                    $sender->sendMessage("§l§cThis player is not online");
+                    $sender->sendMessage("§3§l[§bTpaACM§3] §r§cThis player is not online");
                 }else {
                     if(isset($this->vktmarr[strtolower($player->getLowerCaseName())]) or isset($this->askerarr[strtolower($player->getLowerCaseName())]) or  isset($this->askerarrhere[strtolower($player->getLowerCaseName())]) or  isset($this->vktmarrhere[strtolower($player->getLowerCaseName())])){
                 
@@ -102,27 +110,27 @@ class Tpa extends PluginBase implements Listener{
                        }else{
                     $this->asker = $sender;
                     $this->vktm = $player;
-                    $player->sendMessage("§a§l".$sender->getName() . " sent request to you to teleport to them  \n §l§6/tpaccept for accept §5or §c/tpdeny for deny");
-                    $sender->sendMessage("§a§lYour request as been send to ". $player->getName());
+                    $player->sendMessage("§3§l[§bTpaACM§3] §r§e".$sender->getName() . " §asent request to you to teleport to them  \n §l§6/tpaccept for accept §5or §c/tpadeny for deny");
+                    $sender->sendMessage("§3§l[§bTpaACM§3] §r§aYour request as been send to §e". $player->getName());
                     $this->askerarrhere[$sender->getLowerCaseName()] = true;
                     $this->vktmarrhere[$player->getLowerCaseName()] = true;
                        }
                 }
             }
             break;
-            case "tpdeny":
+            case "tpadeny":
             if(isset($this->vktmarr[strtolower($sender->getLowerCaseName())])){
-                $this->asker->sendMessage("§c§l".$sender->getName() . " deny your request");
-                $this->vktm->sendMessage("§c§lYou have deny a request");
+                $this->asker->sendMessage("§3§l[§bTpaACM§3]§r§e".$sender->getName() . " §cdeny your request");
+                $this->vktm->sendMessage("§3§l[§bTpaACM§3] §c§lYou have deny a request");
                 unset($this->askerarr[$this->asker->getLowerCaseName()]);
                 unset($this->vktmarr[$this->vktm->getLowerCaseName()]);
               }elseif(isset($this->vktmarrhere[strtolower($sender->getLowerCaseName())])){ 
-                  $this->asker->sendMessage("§c§l".$sender->getName() . " deny your request");
-                  $this->vktm->sendMessage("§c§lYou have deny a request");
+                  $this->asker->sendMessage("§3§l[§bTpaACM§3]§r§e".$sender->getName() . " §cdeny your request");
+                  $this->vktm->sendMessage("§3§l[§bTpaACM§3] §c§lYou have deny a request");
                   unset($this->askerarrhere[$this->asker->getLowerCaseName()]);
                   unset($this->vktmarrhere[$this->vktm->getLowerCaseName()]);
               }else{
-                  $sender->sendMessage("§c§lYou dont have request");
+                  $sender->sendMessage("§3§l[§bTpaACM§3] §r§cYou dont have request");
               }
             break;
         }
